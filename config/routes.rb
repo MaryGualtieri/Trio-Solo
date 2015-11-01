f33612db433c4fbab1128b202f07560a94a41d91
PatriciodelaGuardia <delaguardiapatricio@gmail.com>
2015-11-01 12:52:05 -0500

    Delete scaffolds, add User views

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   root  to: 'home#index'
 
   resources :products
-  resources :reviews
-  resources :cart_items
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
